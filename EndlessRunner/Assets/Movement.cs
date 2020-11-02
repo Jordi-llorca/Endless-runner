f3c511e0e6c089494df63f824748a275b9b0caa8
Jordi <jorllorber@gmail.com>
2020-11-02 21:01:44 +0100

    Añadido Movimiento Vertical

--- EndlessRunner/Assets/Movement.cs
+++ EndlessRunner/Assets/Movement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int position = 0;
+    public float distancia = 3f;
+    public float velocidad = 2f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        movimientoVertical();
     }
+
+    void movimientoVertical()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //transform.Translate(Vector3.up * distancia * Time.deltaTime);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, velocidad), ForceMode2D.Impulse);
+            position++;
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * distancia);
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -velocidad/10), ForceMode2D.Impulse);
+            position--;
+        }
+    }
+    
 }
